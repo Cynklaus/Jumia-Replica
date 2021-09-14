1d2c6a26f7d1ce886ac11790fe0af3027ed25ebd
Cynklaus <odhiambocolas@gmail.com>
2021-09-14 07:37:43 +0300

    final replica

---
+++ assets/scripts/app.js
@@ -0,0 +1,22 @@
+// scripts for the sticky full width top nav
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// scripts for sidenav
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
